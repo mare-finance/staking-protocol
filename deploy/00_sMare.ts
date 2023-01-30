aca866728d0a45e007f76aa64b264b7c12b0e77a
gathon <ismlkucuk@gmail.com>
2023-01-30 11:24:04 +0300

    chore: export deploy tags

--- deploy/00_sMare.ts
+++ deploy/00_sMare.ts
@@ -3,7 +3,6 @@ import { DeployFunction } from 'hardhat-deploy/types';
 
 const mareAddress = '0x1db2466d9f5e10d7090e7152b68d62703a2245f0';
 const veloAddress = '0x3c8b650257cfb5f272f799f5e2b4e65093a11a05';
-const usdcAddress = '0x7f5c764cbc14f9669b88837ca1490cca17c31607';
 
 const func: DeployFunction = async ({
     getNamedAccounts,
@@ -13,16 +12,19 @@ const func: DeployFunction = async ({
 }: HardhatRuntimeEnvironment) => {
     const { deployer } = await getNamedAccounts();
 
-    const stakeDeploy = await deploy('uMare', {
+    const stakeDeploy = await deploy('sMare', {
         from: deployer,
         log: true,
         contract: 'contracts/StakedDistributor.sol:StakedDistributor',
-        args: [mareAddress, 'Staked Mare', 'uMare'],
+        args: [mareAddress, 'Staked Mare', 'sMare'],
     });
     const staking = await ethers.getContractAt('StakedDistributor', stakeDeploy.address);
 
-    await (await staking.addToken(usdcAddress)).wait(1);
+    await (await staking.addToken(mareAddress)).wait(1);
     await (await staking.addToken(veloAddress)).wait(1);
 };
+
+const tags = ['sMare'];
+export { tags };
 
 export default func;
